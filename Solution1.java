7939c4f2ce6f2135809ee4bd079ecdbc521a539a
D. Devi Prasad <118455529+G1thub-DP@users.noreply.github.com>
2023-02-18 20:33:46 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,18 @@
+package solution;
+import java.util.*;
+public class Solution1 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int input=sc.nextInt();
+		
+		if(input > 0) {
+			System.out.println("Number is Positive");
+			
+		}else if(input < 0) {
+			System.out.println("Number is Negative");
+		}
+
+	}
+
+}
